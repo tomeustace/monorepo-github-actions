3f7c70308995811340e867f6423b521661ed5ea0
Tom Eustace <tomeustace@gmail.com>
2022-08-05 09:41:16 +0100

    Update to use api main.js for api function entry.  Update actions to deploy app and function.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,19 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+import {ExpressAdapter, NestExpressApplication} from '@nestjs/platform-express';
+import {NestFactory} from '@nestjs/core';
+import * as express from 'express';
+import * as functions from 'firebase-functions';
+import {AppModule} from './app/app.module';
+const server: express.Express = express();
+export const createNestServer = async (expressInstance: express.Express) => {
+  console.log("test action!!")
+  const adapter = new ExpressAdapter(expressInstance);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule, adapter, {},
   );
-}
-
-bootstrap();
+  app.enableCors();
+  return app.init();
+};
+createNestServer(server)
+  .then(v => console.log('Nest Server Ready!!!!!!!'))
+  .catch(err => console.error('Nest broken', err));
+export const api: functions.HttpsFunction = functions.region('europe-west1').https.onRequest(server);
