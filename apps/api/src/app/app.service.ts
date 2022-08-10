ca4ea84682c67cb6c60bcbfe9d1d6e9eb280e48e
Tom Eustace <tomeustace@gmail.com>
2022-08-10 21:22:16 +0100

    Clean up actions

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -4,6 +4,6 @@ import { Message } from '@monorepo-github-actions/api-interfaces';
 @Injectable()
 export class AppService {
   getData(): Message {
-    return { message: 'Welcome to api!!' };
+    return { message: 'Welcome to api!!!' };
   }
 }
